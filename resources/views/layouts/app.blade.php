f8f294f13cad85bcc410c497c2f3dfaf32a4a71d
Songhua Hu <songhua@gmail.com>
2020-04-13 21:32:01 -0700

    Add ui for user management:

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,30 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
-    <link rel="shortcut icon" href="images/favicon.svg" type="image/x-icon" />
-    <meta name="Description" content="Coronavirus Tracker">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Brave BAT</title>
-    @if (env('APP_ENV') == 'production')
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163561589-1"></script>
-        <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        gtag('config', 'UA-163561589-1');
-        </script>
-    @endif
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
-<body class='antialiased'>
-  <div id='app'>
-      @yield('content')
-  </div>
-    <script src="{{mix('/js/app.js')}}"></script>
-    @stack('scripts')
+<body class="bg-gray-100 h-screen antialiased leading-none">
+    <div id="app">
+        @yield('content')
+    </div>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    brave bat
+    Brave Bat
 @endsection
