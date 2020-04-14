15e9265eefae36d445b3bd72c16f485eb9d57404
Songhua Hu <songhua@gmail.com>
2020-04-13 21:50:24 -0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@welcome')->name('welcome');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
