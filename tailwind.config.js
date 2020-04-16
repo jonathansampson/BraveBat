7ccf231104e90cdb860c3ae8675ef9b66f49e680
Songhua Hu <songhua@gmail.com>
2020-04-15 17:20:01 -0700

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'brand-orange': '#fb542b',
+        'brand-dark': '#343546',
+        'brand-gray': '#A0A1B2',
+        'brand-light': '#f0f0f0',
+        'brand-purple': '#a3278f',
+        'brand-blue': '#4f30ab'
+      }
+    }
   },
   variants: {},
   plugins: [
