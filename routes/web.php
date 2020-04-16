0b7960abd6fae11edd5e23308d76874c21b99876
Songhua Hu <songhua@gmail.com>
2020-04-15 17:23:20 -0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@welcome')->name('welcome');
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+// Auth::routes();
+// Route::get('/home', 'HomeController@index')->name('home');
 
 Route::any('/charts/dau', 'ChartDataController@dau')->name('charts.dau');
 Route::any('/charts/bat_purchase', 'ChartDataController@batPurchase')->name('charts.bat_purchase');
