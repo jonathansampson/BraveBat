0a1934d5398bfca7045994f8e05cb186daf3dcb4
Songhua Hu <songhua@gmail.com>
2020-04-22 18:53:48 -0700

    Update

--- resources/views/creators/partials/website.blade.php
+++ resources/views/creators/partials/website.blade.php
@@ -1,7 +1,7 @@
 <div class="max-w-md overflow-hidden rounded shadow-lg">
     <img class="w-full" src="https://bravebat-prod.s3.us-west-2.amazonaws.com/website_screenshots/0ut3r_space.png" alt="{{$creator->name}}">
     <div class="flex items-center justify-between px-4 pt-6">
-        <div class="text-left">
+        <div class="w-1/2 text-left">
             <div class="flex items-center mb-2">
                 <div class="w-4 h-4">
                     @include('partials.icons.'.$creator->channel)
@@ -12,7 +12,7 @@
             </div>    
             <div class="mb-2 text-sm text-gray-500 capitalize">{{$creator->channel}}</div>    
         </div>
-        <div class="flex-shrink-0 ">
+        <div class="flex-shrink-0 w-1/2 text-right">
             <x-verified-badge />
         </div>
     </div>
