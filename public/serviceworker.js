2b2b891782ea080bee23507ef1cdc50b1c70efc7
Songhua Hu <songhua@gmail.com>
2020-04-25 14:13:25 -0700

    Roll out PWA

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -3,14 +3,12 @@ var filesToCache = [
     '/offline',
     '/css/app.css',
     '/js/app.js',
-    '/images/icons/icon-72x72.png',
-    '/images/icons/icon-96x96.png',
-    '/images/icons/icon-128x128.png',
-    '/images/icons/icon-144x144.png',
-    '/images/icons/icon-152x152.png',
-    '/images/icons/icon-192x192.png',
-    '/images/icons/icon-384x384.png',
-    '/images/icons/icon-512x512.png',
+    '/images/logos/apple-icon-120.png',
+    '/images/logos/apple-icon-152.png',
+    '/images/logos/apple-icon-167.png',
+    '/images/logos/apple-icon-180.png',
+    '/images/logos/manifest-icon-192.png',
+    '/images/logos/manifest-icon-512.png',
 ];
 
 // Cache on install
