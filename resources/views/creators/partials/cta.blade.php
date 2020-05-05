d24a2b734fc91b3227a7b733f587f6cb6aeb1c2f
Songhua Hu <songhua@gmail.com>
2020-05-05 16:11:07 -0700

    update

--- resources/views/creators/partials/cta.blade.php
+++ resources/views/creators/partials/cta.blade.php
@@ -2,6 +2,6 @@
     <ol class="mx-4 mb-4 list-inside">
         <li class="mt-4 list-decimal ">Download <a href="{{config('bravebat.referral_link')}}" class="underline">Brave Browser</a></li>
         <li class="mt-4 list-decimal "><a href="https://brave.com/tips/" class="underline">Leave a tip</a> or <a href="https://brave.com/brave-rewards/" class="underline">make regular controbutions</a></li>
-        <li class="mt-4 list-decimal ">Thank you!</li>
+        <li class="mt-4 list-decimal ">Thank you! 👏👏👏</li>
     </ol>
 </div>
