b624d2f98845f23cc831f4d63ec8fb1236e7739b
Songhua Hu <songhua@gmail.com>
2020-05-11 13:38:27 -0700

    update

--- resources/views/creators/partials/website.blade.php
+++ resources/views/creators/partials/website.blade.php
@@ -1,5 +1,8 @@
 <div class="max-w-md overflow-hidden border rounded shadow-lg">
-    <img class="w-full" src="https://bravebat-prod.s3.us-west-2.amazonaws.com/{{$creator->screenshot}}" alt="{{$creator->name}}">
+    @if ($creator->screenshot)
+    <img class="w-full" src="https://bravebat-prod.s3.us-west-2.amazonaws.com/{{$creator->screenshot}}"
+        alt="{{$creator->name}}">
+    @endif
     <div class="flex items-center justify-between px-4 pt-6">
         <div class="text-left">
             <div class="flex items-center mb-2">
@@ -9,14 +12,14 @@
                 <h1 class="ml-2 text-lg">
                     <a href="{{$creator->link}}" target='_blank' class="text-blue-500">{{$creator->name}}</a>
                 </h1>
-            </div>    
-            <div class="mb-2 text-sm text-gray-500 capitalize">{{$creator->channel}}</div>    
+            </div>
+            <div class="mb-2 text-sm text-gray-500 capitalize">{{$creator->channel}}</div>
         </div>
         <div class="text-right">
             <h1 class="mb-2 text-lg">
                 {{number_format($creator->alexa_ranking)}}
             </h1>
-            <div class="mb-2 text-sm text-gray-500 capitalize">Alexa Rank</div>    
+            <div class="mb-2 text-sm text-gray-500 capitalize">Alexa Rank</div>
         </div>
     </div>
     <div class="px-4 pt-4 pb-6">
