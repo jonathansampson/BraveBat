41385acaa992cf0e0d79a70e63078ea26cb61c29
Songhua Hu <songhua@gmail.com>
2020-05-17 15:42:35 -0700

    Update

--- resources/views/partials/channel_charts.blade.php
+++ resources/views/partials/channel_charts.blade.php
@@ -3,17 +3,27 @@
         @includeIf('partials.icons.'.$channel)
     </div>
     <div>
+        @if ($channel == 'reddit')
         {{round($creator_count[$channel] / 1000, 1) }}K
         Brave Creators
+        @else
+        <a href="{{route('creators.index', ['channel' => $channel])}}">
+            {{round($creator_count[$channel] / 1000, 1) }}K
+            Brave Creators
+        </a>
+        @endif
+
     </div>
 </div>
 <div class="flex flex-wrap">
     <div class="w-full lg:w-1/2">
         <h2 class="py-8 text-xl font-semibold text-center">Total</h2>
-        @include('charts.line', ['id' => 'creator_daily_total_stats/'.$channel, 'brand_color' => $channel, 'unit' => 'day'])
+        @include('charts.line', ['id' => 'creator_daily_total_stats/'.$channel, 'brand_color' => $channel, 'unit' =>
+        'day'])
     </div>
     <div class="w-full lg:w-1/2">
         <h2 class="py-8 text-xl font-semibold text-center">Daily Addition</h2>
-        @include('charts.line', ['id' => 'creator_daily_addition_stats/'.$channel, 'brand_color' => $channel, 'unit' => 'day'])
+        @include('charts.line', ['id' => 'creator_daily_addition_stats/'.$channel, 'brand_color' => $channel, 'unit' =>
+        'day'])
     </div>
 </div>
