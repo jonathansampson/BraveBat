f2a66b93bc125b0f58e9b8d6760d575a19e57cf8
Songhua Hu <songhua@gmail.com>
2020-07-16 10:39:30 -0700

    Update

--- config/horizon.php
+++ config/horizon.php
@@ -56,7 +56,7 @@ return [
 
     'prefix' => env(
         'HORIZON_PREFIX',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_horizon:'
+        Str::slug(env('APP_NAME', 'laravel'), '_') . '_horizon:'
     ),
 
     /*
@@ -170,7 +170,7 @@ return [
                 'connection' => 'redis',
                 'queue' => ['default'],
                 'balance' => 'simple',
-                'processes' => 10,
+                'processes' => 3,
                 'tries' => 1,
             ],
         ],
