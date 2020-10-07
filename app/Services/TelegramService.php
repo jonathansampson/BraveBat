d2e79510810d31bb4c66427eace32ea622df976e
Songhua Hu <songhua@gmail.com>
2020-10-07 10:40:38 -0700

    Lasso & Deploy

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -8,15 +8,14 @@ class TelegramService
 {
     public function getChatMembersCount($community)
     {
-
         $url = "https://api.telegram.org/bot" . config('services.telegram.bot_token') . '/getChatMembersCount?chat_id=@' . $community;
         $response = Http::get($url);
         if ($response->ok()) {
             return [
                 'success' => true,
                 'result' => [
-                    'subscribers' => $response->json()['result']
-                ]
+                    'subscribers' => $response->json()['result'],
+                ],
             ];
         }
     }
