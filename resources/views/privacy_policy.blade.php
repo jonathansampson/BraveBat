eabae5db8c8b0c208fc4bc8b9b2f258d7663afd5
Songhua Hu <songhua@gmail.com>
2020-11-25 13:50:40 -0800

    Deploy

--- resources/views/privacy_policy.blade.php
+++ resources/views/privacy_policy.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('title', 'BraveBat Privacy Policy')
-@section('description', 'Your comprehensive resource on Brave Browser, Basic Attention Token and their passionate communities')
+@section('description', 'Your comprehensive resource on Brave Browser, Basic Attention Token and their passionate
+communities')
 
 @section('content')
 <div class="container px-4 pb-8 mx-auto sm:px-6 md:px-8">
@@ -10,22 +11,31 @@
     </div>
     <div class="leading-6">
         <p class="pt-4">
-            BraveBat is not in the business of making money off your data, and we do not serve ads. Everything we collect is used exclusively to help us provide you with the best service possible.
+            BraveBat is not in the business of making money off your data, and we do not serve ads. Everything we
+            collect is used exclusively to help us provide you with the best service possible.
         </p>
         <h2 class="pt-6 text-lg font-semibold">
             Identity & Access
         </h2>
         <p class="pt-4">
-            When you sign up for BraveBat, we ask for your name and email address. That’s just so you can personalize your new account. We’ll never sell or rent your personal info to third parties, and we won’t use your name or company in marketing statements without your permission, either.
+            When you sign up for BraveBat, we ask for your name and email address. That’s just so you can personalize
+            your new account. We’ll never sell or rent your personal info to third parties, and we won’t use your name
+            or company in marketing statements without your permission, either.
         </p>
         <p class="pt-4">
-            When you contact BraveBat with a question or to ask for help, we’ll keep that correspondence, and the email address, for future reference. When you browse our marketing pages, we’ll track that for statistical purposes (like conversion rates and to test new designs). We also store any information you volunteer, like surveys, for as long as it makes sense.
+            When you contact BraveBat with a question or to ask for help, we’ll keep that correspondence, and the email
+            address, for future reference. When you browse our marketing pages, we’ll track that for statistical
+            purposes (like conversion rates and to test new designs). We also store any information you volunteer, like
+            surveys, for as long as it makes sense.
         </p>
         <h2 class="pt-6 text-lg font-semibold">
             Your Rights With Respect to Your Information
         </h2>
         <p class="pt-4">
-            You may have heard about the General Data Protection Regulation (“GDPR”) in Europe. GDPR gives people under its protection certain rights with respect to their personal information collected by us on the Site. Accordingly, BraveBat recognizes and will comply with GDPR and those rights, except as limited by applicable law. The rights under GDPR include:
+            You may have heard about the General Data Protection Regulation (“GDPR”) in Europe. GDPR gives people under
+            its protection certain rights with respect to their personal information collected by us on the Site.
+            Accordingly, BraveBat recognizes and will comply with GDPR and those rights, except as limited by applicable
+            law. The rights under GDPR include:
         </p>
         <ul class="pt-4 list-inside">
             <li class="list-disc">Right of Access.</li>
@@ -39,9 +49,11 @@
         </ul>
         <h2 class="pt-6 text-lg font-semibold">
             Processors we use
-        </h2> 
+        </h2>
         <p class="pt-4">
-            As part of the services we provide, we use certain third party processors to process information of Brave Browser's verified creators. We do <span class="font-bold">NOT</span> use these processors to process your data.
+            As part of the services we provide, we use certain third party processors to process information of Brave
+            Browser's verified creators. We do <span class="font-bold">NOT</span> use these processors to process your
+            data.
         </p>
         <ul class="pt-4 list-inside">
             <li class="list-disc">Google YouTube API Services
@@ -51,23 +63,27 @@
                 (<a href="https://www.twitch.tv/p/legal/privacy-notice/" class="text-blue-500">Privacy Policy</a>)
             </li>
             <li class="list-disc">GitHub API Services
-                (<a href="https://help.github.com/en/github/site-policy/github-privacy-statement" class="text-blue-500">Privacy Policy</a>)
+                (<a href="https://help.github.com/en/github/site-policy/github-privacy-statement"
+                    class="text-blue-500">Privacy Policy</a>)
             </li>
             <li class="list-disc">Twitter API Services
-                (<a href="https://help.twitter.com/en/rules-and-policies/personal-information" class="text-blue-500">Privacy Policy</a>)
+                (<a href="https://help.twitter.com/en/rules-and-policies/personal-information"
+                    class="text-blue-500">Privacy Policy</a>)
             </li>
             <li class="list-disc">Vimeo API Services
                 (<a href="https://vimeo.com/privacy" class="text-blue-500">Privacy Policy</a>)
             </li>
             <li class="list-disc">Reddit API Services
-                (<a href="https://www.redditinc.com/policies/privacy-policy-january-10-2020" class="text-blue-500">Privacy Policy</a>)
+                (<a href="https://www.redditinc.com/policies/privacy-policy-january-10-2020"
+                    class="text-blue-500">Privacy Policy</a>)
             </li>
             <li class="list-disc">Brave Creator API Services
                 (<a href="https://brave.com/publishers-creators-privacy/" class="text-blue-500">Privacy Policy</a>)
             </li>
         </ul>
         <p class="pt-4">
-            As part of the services we provide, and only to the extent necessary, we may use certain third party processors to process some or all of your personal information.
+            As part of the services we provide, and only to the extent necessary, we may use certain third party
+            processors to process some or all of your personal information.
         </p>
         <ul class="pt-4 list-inside">
             <li class="list-disc">CloudFlare
@@ -79,35 +95,42 @@
         </ul>
         <h2 class="pt-6 text-lg font-semibold">
             Law enforcement
-        </h2> 
+        </h2>
         <p class="pt-4">
-            BraveBat won’t hand your data over to law enforcement unless a court order says we have to. We flat-out reject requests from local and federal law enforcement when they seek data without a court order. And unless we’re legally prevented from it, we’ll always inform you when such requests are made.
+            BraveBat won’t hand your data over to law enforcement unless a court order says we have to. We flat-out
+            reject requests from local and federal law enforcement when they seek data without a court order. And unless
+            we’re legally prevented from it, we’ll always inform you when such requests are made.
         </p>
-            
+
         <h2 class="pt-6 text-lg font-semibold">
             Security & Encryption
-        </h2> 
+        </h2>
         <p class="pt-4">
-            All data is encrypted via SSL/TLS when transmitted from our servers to your browser. Data isn’t encrypted while it’s live in our database (since it needs to be ready to send to you when you need it), but we go to great lengths to secure your data at rest. 
+            All data is encrypted via SSL/TLS when transmitted from our servers to your browser. Data isn’t encrypted
+            while it’s live in our database (since it needs to be ready to send to you when you need it), but we go to
+            great lengths to secure your data at rest.
         </p>
         <h2 class="pt-6 text-lg font-semibold">
             Location of Site and Data
-        </h2> 
+        </h2>
         <p class="pt-4">
-            This Site is operated in the United States. If you are located in the European Union or elsewhere outside of the United States, please be aware that any information you provide to us will be transferred to the United States. By using our Site, participating in any of our services and/or providing us with your information, you consent to this transfer. 
+            This Site is operated in the United States. If you are located in the European Union or elsewhere outside of
+            the United States, please be aware that any information you provide to us will be transferred to the United
+            States. By using our Site, participating in any of our services and/or providing us with your information,
+            you consent to this transfer.
         </p>
         <h2 class="pt-6 text-lg font-semibold">
             Changes & questions
-        </h2> 
+        </h2>
         <p class="pt-4">
-            BraveBat may update this policy. We’ll notify you about significant changes by emailing the account owner. You can access, change or delete your personal information at any time by contacting our support team.
+            BraveBat may update this policy. We’ll notify you about significant changes by emailing the account owner.
+            You can access, change or delete your personal information at any time by contacting our support team.
         </p>
         <p class="pt-4">
-            Questions about this privacy policy? Please get in touch and we’ll be happy to answer them! Our email is support@bravebat.info.
+            Questions about this privacy policy? Please get in touch and we’ll be happy to answer them! Our email is
+            bravebatinfo@gmail.com.
         </p>
     </div>
 
 </div>
 @endsection
-
-
