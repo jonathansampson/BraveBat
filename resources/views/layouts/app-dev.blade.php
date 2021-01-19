ea691b17aa530022daef54c830a0ae64863d3e99
Songhua Hu <songhua@gmail.com>
2021-01-19 14:40:05 -0800

    Deploy

--- resources/views/layouts/app-dev.blade.php
+++ resources/views/layouts/app-dev.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,27 +9,28 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>BraveBat | @yield('title' , "Resource on Brave Browser, BAT and Beyond")</title>
-    <meta name="description" content="@yield('description', 'Your comprehensive resource on Brave Browser, Basic Attention Token and their passionate communities')"/>
-    
+    <meta name="description"
+        content="@yield('description', 'Your comprehensive resource on Brave Browser, Basic Attention Token and their passionate communities')" />
+
     <link rel="shortcut icon" href="/images/favicon.svg" type="image/x-icon" />
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     @stack('styles')
-    
-    @livewireStyles
+
     @laravelPWA
 
     @if (app()->environment('production'))
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163561589-1"></script>
-        <script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163561589-1"></script>
+    <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', 'UA-163561589-1');
-        </script>
+    </script>
     @endif
 </head>
+
 <body class="antialiased leading-6 text-brand-dark">
     <div id="app" class="flex flex-col min-h-screen">
         @include('partials.nav-dev')
@@ -40,8 +42,7 @@
     </div>
     <script src="{{ mix('js/app.js') }}"></script>
 
-    @livewireScripts
     @stack('scripts')
 </body>
-</html>
 
+</html>
