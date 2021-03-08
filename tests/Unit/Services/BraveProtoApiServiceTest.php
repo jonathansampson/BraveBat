05511c146dfcbc82cec2c7586df3e9cbf1de45c5
Songhua Hu <songhua@gmail.com>
2021-03-07 16:23:50 -0800

    Commit the Brave API test file

--- tests/Unit/Services/BraveProtoApiServiceTest.php
+++ tests/Unit/Services/BraveProtoApiServiceTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Unit\Services;
 
-use Publishers_pb\ChannelResponse;
+use Publishers_pb\ChannelResponseList;
 use Tests\TestCase;
 
 class BraveProtoApiServiceTest extends TestCase
@@ -13,16 +13,43 @@ class BraveProtoApiServiceTest extends TestCase
      */
     public function it_tries_to_get_a_channel_response_based_on_a_prefix()
     {
-        // Reach out to CDN for a channel response based on prefix of 12e3
-        $data = file_get_contents("https://pcdn.brave.com/publishers/prefixes/12e3");
+        // Stream the file from https://pcdn.brave.com/publishers/prefixes/12e3
+        $fp = fopen("https://pcdn.brave.com/publishers/prefixes/12e3", 'rb');
 
-        // Initiate a class object of ChannelResponse. This is successful.
-        $channelResponse = new ChannelResponse();
+        // Get the first 4 bytes, convert from binary to hex and then from hex to demical
+        $header = fread($fp, 4);
+        $length = hexdec(bin2hex($header)); // $length = 852
 
-        // Attempt to load data into the ChannelResponse object. This is successful.
-        $channelResponse->mergeFromString($data);
+        // Read the rest of the file and use Brotli to uncompress it
+        $body = fread($fp, $length);
+        $response = brotli_uncompress($body);
 
-        // Try to get the channel identifier as a test. This is the step when it return null. I suspect that the data file has been compressed
-        echo $channelResponse->getChannelIdentifier();
+        // Initiate a class object of ChannelResponseList and inject the API data into the object
+        $channelResponseList = new ChannelResponseList();
+        $channelResponseList->mergeFromString($response);
+
+        // Loop through the list array and properly get the result. SUCCESS!
+        foreach ($channelResponseList->getChannelResponses() as $channelResponse) {
+            echo ($channelResponse->getChannelIdentifier());
+            echo "\n";
+        }
+    }
+
+    /**
+     * @test
+     * @group api
+     */
+    public function it_tries_to_get_prefix_list()
+    {
+        // Stream the file from https://rewards.brave.com/publishers/prefix-list
+        $fp = fopen("https://rewards.brave.com/publishers/prefix-list", 'rb');
+
+        // Get the first 4 bytes, convert from binary to hex and then from hex to demical
+        $header = fread($fp, 4);
+        $length = hexdec(bin2hex($header)); // $length = 134_483_969 (this does not seem to make sense because it is much bigger than the file byte size)
+
+        ini_set('memory_limit', '-1');
+        $body = fread($fp, $length);
+        $response = brotli_uncompress($body); // FAIL here. Message: Brotli decompress failed
     }
 }
