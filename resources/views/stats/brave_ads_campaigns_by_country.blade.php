317a02562d2c050969dffab777c62ece367b066c
Songhua Hu <songhua@gmail.com>
2021-03-31 14:38:53 -0700

    Lasso & Deploy

--- resources/views/stats/brave_ads_campaigns_by_country.blade.php
+++ resources/views/stats/brave_ads_campaigns_by_country.blade.php
@@ -8,7 +8,7 @@
     <div class="flex flex-wrap">
         <div class="w-full">
             <toggleable-line-chart url="{{'/charts/active_ad_campaigns?country=' . $country}}"
-                title="{{"Brave Active Ads Campaigns in " . $country}}" :toggleable="false">
+                title="{{"Brave Active Ads Campaigns in " . $country}}">
             </toggleable-line-chart>
         </div>
     </div>
