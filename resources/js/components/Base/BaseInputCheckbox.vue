06a6b27c5fb20acab901fb5b24fa7c880837f901
Songhua Hu <songhua@gmail.com>
2021-04-05 14:20:00 -0700

    fix

--- resources/js/components/Base/BaseInputCheckbox.vue
+++ resources/js/components/Base/BaseInputCheckbox.vue
@@ -3,6 +3,7 @@
     <input
       type="checkbox"
       :name="name"
+      :id="name"
       :checked="checked"
       @change="onChange"
       class="mr-2 border-gray-300 rounded shadow-sm text-brand-orange focus:border-yellow-400 focus:ring focus:ring-offset-0 focus:ring-yellow-200 focus:ring-opacity-50"
