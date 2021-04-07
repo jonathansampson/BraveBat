0726f74c5bd1982a9eda3ffc61ceb047cbeb8c27
Songhua Hu <songhua@gmail.com>
2021-04-06 20:16:03 -0700

    fix

--- app/Http/Controllers/Api/ApiResponseTrait.php
+++ app/Http/Controllers/Api/ApiResponseTrait.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 trait ApiResponseTrait
 {
+
     public static function missing_field_response()
     {
         return response()->json(['success' => false, 'message' => "Missing required field"], 422);
