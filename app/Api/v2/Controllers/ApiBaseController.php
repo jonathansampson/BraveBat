080ad7c842a18da897fc63f1d39bbfc7c6b27c37
Songhua Hu <songhua@gmail.com>
2021-04-07 20:04:54 -0700

    update

--- app/Api/v2/Controllers/ApiBaseController.php
+++ app/Api/v2/Controllers/ApiBaseController.php
@@ -4,8 +4,7 @@ namespace App\Api\v2\Controllers;
 
 /**
  * @OA\Info(title="BraveBat API", version="2.0")
- * @OA\Server(url="http://bravebat.test/api/v2/", description="Local Server")
- * @OA\Server(url="https://bravebat.info/api/v2/", description="Production Server")
+ * @OA\Server(url="https://bravebat.info/api/v2/")
  */
 use App\Http\Controllers\Controller;
 
