f0ca7de0ebfadc53b8ae2737752d55c2757acb3f
Songhua Hu <songhua@gmail.com>
2021-04-12 10:46:13 -0700

    bug fix

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -24,15 +24,15 @@ class GenerateSitemap extends Command
         $fullScan = $this->option('full');
 
         SitemapIndex::create(config('app.url'))
-            ->add(self::CONSTANT . '/app.xml')
-            ->add(self::CONSTANT . '/creators.xml')
+            ->add(self::FOLDER . '/app.xml')
+            ->add(self::FOLDER . '/creators.xml')
             ->writeToFile(public_path('sitemap.xml'));
 
         SitemapGenerator::create(config('app.url'))
             ->configureCrawler(function (Crawler $crawler) {
                 $crawler->setMaximumDepth(1);
             })
-            ->writeToFile(public_path(self::CONSTANT . '/app.xml'));
+            ->writeToFile(public_path(self::FOLDER . '/app.xml'));
 
         if ($fullScan) {
             $this->generateCreatorSitemapIndex();
@@ -46,13 +46,13 @@ class GenerateSitemap extends Command
         $sitemapIndex = SitemapIndex::create(config('app.url'));
 
         for ($i = 0; $i < $filesCount; $i++) {
-            $sitemapIndex->add(self::CONSTANT . "/creators_{$i}.xml");
+            $sitemapIndex->add(self::FOLDER . "/creators_{$i}.xml");
         }
-        $sitemapIndex->writeToFile(public_path(self::CONSTANT . "/creators.xml"));
+        $sitemapIndex->writeToFile(public_path(self::FOLDER . "/creators.xml"));
 
         for ($i = 0; $i < $filesCount; $i++) {
             $sitemap = Sitemap::create(config('app.url'));
-            $fileName = self::CONSTANT . "/creators_{$i}.xml";
+            $fileName = self::FOLDER . "/creators_{$i}.xml";
             $creators = Creator::where('id', ">=", $i * 1000)->where("id", "<", ($i + 1) * 1000)->get();
             foreach ($creators as $creator) {
                 $sitemap->add(Url::create(route('creators.show', [$creator->channel, $creator->id]))
