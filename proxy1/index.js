7854981c530d233200a3881d224f29f87f292272
Songhua Hu <songhua@gmail.com>
2021-04-15 15:11:46 -0700

    add proxy

--- proxy1/index.js
+++
@@ -1,8 +0,0 @@
-addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-  const resp = await fetch('https://ads-serve.brave.com/v7/catalog')
-  return resp
-}
